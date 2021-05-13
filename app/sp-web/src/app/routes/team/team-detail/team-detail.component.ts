283d425b6402eee64a5e9d7c16a5ea49441d30b7
Martin Filo <martinxfilo@gmail.com>
2021-05-13 12:36:05 +0200

    #6 - team - add member to team

--- app/sp-web/src/app/routes/team/team-detail/team-detail.component.ts
+++ app/sp-web/src/app/routes/team/team-detail/team-detail.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
-import { TeamDTO } from '../../../models/sp-api';
+import { NgForm } from '@angular/forms';
+import { CustomHttpError, ErrorResponse, TeamDTO } from '../../../models/sp-api';
 
 @Component({
   selector: 'sp-team-detail',
   templateUrl: './team-detail.component.html',
 })
 export class TeamDetailComponent {
+  @ViewChild('ngForm')
+  private ngForm: NgForm;
+
   public team: TeamDTO;
+
+  addTeammate: boolean = false;
+  newMail: string = '';
+  error: string = '';
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {
     this.route.params.subscribe((p) => {
@@ -16,9 +24,31 @@ export class TeamDetailComponent {
     });
   }
 
-  private getTeamById(id: number): void {
+  private getTeamById(id: string): void {
     this.http.get<TeamDTO>(`api/team/${id}`).subscribe((data) => {
       this.team = data;
+    });
+  }
+
+  add(): void {
+    this.error = null;
+
+    if (this.ngForm.invalid) {
+      this.error = 'Vyplňte všetky povinné údaje';
+      return;
+    }
+
+    this.http.post(`api/team/${this.team.id}/add`, { user_mail: this.newMail }).subscribe({
+      next: () => {
+        this.newMail = '';
+        this.error = '';
+        this.addTeammate = false;
+
+        this.getTeamById(this.team.id);
+      },
+      error: (err: CustomHttpError<ErrorResponse>) => {
+        this.error = err.error.error.message;
+      },
     });
   }
 }
