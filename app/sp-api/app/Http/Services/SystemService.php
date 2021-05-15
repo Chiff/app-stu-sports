d55eef97a2c95f6b53b34de8d71cb9818e120739
Martin Filo <martinxfilo@gmail.com>
2021-05-15 19:27:05 +0200

    #25 - dashboard - myEvents

--- app/sp-api/app/Http/Services/SystemService.php
+++ app/sp-api/app/Http/Services/SystemService.php
@@ -3,33 +3,18 @@
 
 namespace App\Http\Services;
 
-use App\Http\Services\Netgrif\AuthenticationService;
-use App\Http\Services\Netgrif\TaskService;
 use App\Http\Services\Netgrif\WorkflowService;
 use App\Models\Netgrif\CaseResource;
-use App\Models\Netgrif\EmbeddedTasks;
 use App\Models\Netgrif\EmbededCases;
-use App\Models\Netgrif\TaskReference;
-use App\Models\User;
-use JsonMapper\JsonMapper;
 
 class SystemService
 {
-    private AuthenticationService $auth;
-    private JsonMapper $mapper;
     private WorkflowService $workflowService;
-    private TaskService $taskService;
 
     public function __construct(
-        AuthenticationService $authService,
         WorkflowService $workflowService,
-        TaskService $taskService,
-        JsonMapper $mapper,
     ) {
-        $this->mapper = $mapper;
         $this->workflowService = $workflowService;
-        $this->auth = $authService;
-        $this->taskService = $taskService;
     }
 
 
