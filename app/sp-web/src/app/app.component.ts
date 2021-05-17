a82db2a92637f875de1d6dd942c574c299f08aba
Martin Filo <martinxfilo@gmail.com>
2021-05-18 00:16:34 +0200

    #51 - cajocka sa hybe

--- app/sp-web/src/app/app.component.ts
+++ app/sp-web/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, ResolveEnd, Router } from '@angular/router';
 import { AuthService } from './shared/shared/services/auth.service';
 
@@ -6,38 +6,47 @@ import { AuthService } from './shared/shared/services/auth.service';
   selector: 'sp-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   public isTick = false;
   private willTick = false;
   private tickTimeout = 4000;
   public ready = false;
 
+  private t1: number;
+  private t2: number;
+
   constructor(public auth: AuthService, private router: Router) {
     this.router.events.subscribe((e) => {
       if (e instanceof ResolveEnd) {
-        console.warn(this.router.url);
         if (e.urlAfterRedirects === `/dashboard`) {
           setTimeout(() => {
             this.ready = true;
           }, 1000);
         } else {
-          console.warn(e);
           this.ready = false;
           this.isTick = false;
           this.willTick = false;
+
+          clearTimeout(this.t1);
+          clearTimeout(this.t2);
         }
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.t1);
+    clearTimeout(this.t2);
   }
 
   pageActive(event: string) {
     if (event === 'dashboard' && !this.isTick && !this.willTick) {
       // eslint-disable-next-line @typescript-eslint/no-this-alias
       this.willTick = true;
-      console.warn('start');
-      setTimeout(() => {
+
+      this.t1 = setTimeout(() => {
         this.isTick = true;
-        setTimeout(() => {
+        this.t2 = setTimeout(() => {
           this.isTick = false;
           this.willTick = false;
         }, this.tickTimeout);
