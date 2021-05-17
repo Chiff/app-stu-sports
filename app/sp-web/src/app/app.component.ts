d71aded7304ee1b72b5612a3a72bd2e9f3070592
Martin Filo <martinxfilo@gmail.com>
2021-05-17 23:51:57 +0200

    #51 - cajocka sa hybe

--- app/sp-web/src/app/app.component.ts
+++ app/sp-web/src/app/app.component.ts
@@ -7,9 +7,26 @@ import { AuthService } from './shared/shared/services/auth.service';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  public isTick = false;
+  private willTick = false;
+  private tickTimeout = 4000;
+
   constructor(public auth: AuthService, private router: Router) {}
 
   pageActive(event: string) {
+    if (event === 'dashboard' && !this.isTick && !this.willTick) {
+      // eslint-disable-next-line @typescript-eslint/no-this-alias
+      this.willTick = true;
+      console.warn('start');
+      setTimeout(() => {
+        this.isTick = true;
+        setTimeout(() => {
+          this.isTick = false;
+          this.willTick = false;
+        }, this.tickTimeout);
+      }, this.tickTimeout);
+    }
+
     return this.router.url.startsWith(`/${event}`);
   }
 }
